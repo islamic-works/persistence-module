146f7232e6b4917b235d6075903000195c240928
Carlos Delfino - Sony 1 <consultoria@carlosdelfino.eti.br>
2019-09-09 13:46:02 -0300

    otimização no carregamento e descarte do serviço

--- persistence.service.ts
+++ persistence.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
 import { GPSInfo } from '../entities/gps-info';
@@ -11,12 +11,12 @@ import { IslamicUser } from '../auth/islamic-user';
 @Injectable({
     providedIn: 'root'
 })
-export class PersistenceService {
+export class PersistenceService implements OnDestroy{
     constructor(
         protected _settings: SettingsService
     ) {
         this._settings.debug && console.log("PersistenceService.new");
-
+        //firebase.enableLogging(this._settings.debug);
     }
 
     async init() {
@@ -29,17 +29,19 @@ export class PersistenceService {
                 firebase.keepInSync("/users", true).then(() => {
 
                     var onValueEvent = (result: firebase.FBData) => {
+                        console.log("result: " + JSON.stringify(result));
                         console.log("Event type: " + result.type);
                         console.log("Key: " + result.key);
                         console.log("Value: " + JSON.stringify(result.value));
                     };
 
-                    firebase.addChildEventListener(onValueEvent, "/users").then(
+                    return firebase.addChildEventListener(onValueEvent, "/users").then(
                         (listenerWrapper: firebase.AddEventListenerResult) => {
-                            this._settings.debug && console.log("PersistenceService.init firebase.init.then firebase.addChildEventListener");
+                            this._settings.debug && console.log("PersistenceService.init firebase.init.then firebase.addChildEventListener", listenerWrapper);
                             var path = listenerWrapper.path;
                             var listeners = listenerWrapper.listeners; // an Array of listeners added
                             // you can store the wrapper somewhere to later call 'removeEventListeners'
+                            return;
                         }
                     ).catch((error) => console.log("PersistenceService.init firebase.init.then firebase.addChildEventListener.catch", error));
                 });
@@ -53,7 +55,11 @@ export class PersistenceService {
             })
     }
 
-    async close() {
+    async ngOnDestroy() {
+        return this.close();
+    }
+
+    async close(){
         return Promise.resolve();
     }
 
